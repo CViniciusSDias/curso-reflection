dff327a55af6a54dd973c110e2eda01584de931f
Vinicius Dias <carlosv775@gmail.com>
2020-09-05 15:08:10 -0300

    Vídeo 3.4

--- reflection.php
+++ reflection.php
@@ -7,8 +7,20 @@ require_once __DIR__ . '/vendor/autoload.php';
 $reflectionClass = new ReflectionClass(ClasseExemplo::class);
 
 $reflectionMethod = $reflectionClass->getMethod('metodoPublico');
-var_dump($reflectionMethod->getParameters());
+$parameters = array_filter(
+    $reflectionMethod->getParameters(),
+    fn (ReflectionParameter $parameter) => !$parameter->isOptional()
+);
+
+foreach ($parameters as $parameter) {
+    if (!$parameter->hasType()) {
+        throw new DomainException('Não sei o que fazer aqui');
+    }
+
+    $tipo = (string) $parameter->getType();
+    var_dump($tipo, $parameter->getType()->isBuiltin());
+}
 
 $objetoClasseExemplo = $reflectionClass->newInstanceWithoutConstructor();
 
-$reflectionMethod->invokeArgs($objetoClasseExemplo, ['Mensagem qualquer ', 42]);
+// $reflectionMethod->invokeArgs($objetoClasseExemplo, ['Mensagem qualquer ', 42]);
